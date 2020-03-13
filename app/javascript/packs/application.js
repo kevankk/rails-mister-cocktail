4a5489361718d235067a99c105c0853a8f019a28
Kevan Koh <kevankohjinwen@gmail.com>
2020-03-13 17:59:29 +0700

    nope was not javascript function

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,18 +10,18 @@ const initSelect2 = () => {
 };
 
 
-// const input = document.querySelector('#search')
-// input.addEventListener('keyup', (e) => {
-//   const user_input = e.target.value;
-//   document.querySelectorAll('.card-container').forEach(c => {
-//     const cocktail = c.innerText.toLowerCase();
-//     if (cocktail.match(user_input) === null) {
-//       c.classList.add('hidden');
-//     } else {
-//       c.classList.remove('hidden');
-//     }
-// })
-// })
+const input = document.querySelector('#search')
+input.addEventListener('keyup', (e) => {
+  const user_input = e.target.value;
+  document.querySelectorAll('.card-container').forEach(c => {
+    const cocktail = c.innerText.toLowerCase();
+    if (cocktail.match(user_input) === null) {
+      c.classList.add('hidden');
+    } else {
+      c.classList.remove('hidden');
+    }
+})
+})
 
 
 initSelect2();
