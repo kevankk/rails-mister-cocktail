c85af040c48bdd6ad429a30bccebbbee93b62fec
Kevan Koh <kevankohjinwen@gmail.com>
2020-03-13 17:54:21 +0700

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,29 +1,29 @@
-import $ from 'jquery';
-import 'select2';
+// import $ from 'jquery';
+// import 'select2';
 
-import { loadDynamicBannerText } from './components/type';
-import 'select2/dist/css/select2.css';
-
-
-const initSelect2 = () => {
-  $('.select2').select2();
-};
+// import { loadDynamicBannerText } from './components/type';
+// import 'select2/dist/css/select2.css';
 
 
-const input = document.querySelector('#search')
-input.addEventListener('keyup', (e) => {
-  const user_input = e.target.value;
-  document.querySelectorAll('.card-container').forEach(c => {
-    const cocktail = c.innerText.toLowerCase();
-    if (cocktail.match(user_input) === null) {
-      c.classList.add('hidden');
-    } else {
-      c.classList.remove('hidden');
-    }
-})
-})
+// const initSelect2 = () => {
+//   $('.select2').select2();
+// };
 
 
-initSelect2();
-loadDynamicBannerText();
+// const input = document.querySelector('#search')
+// input.addEventListener('keyup', (e) => {
+//   const user_input = e.target.value;
+//   document.querySelectorAll('.card-container').forEach(c => {
+//     const cocktail = c.innerText.toLowerCase();
+//     if (cocktail.match(user_input) === null) {
+//       c.classList.add('hidden');
+//     } else {
+//       c.classList.remove('hidden');
+//     }
+// })
+// })
+
+
+// initSelect2();
+// loadDynamicBannerText();
 
